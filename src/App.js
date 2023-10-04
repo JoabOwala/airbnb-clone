db5f9233907ed1461f8dff0b52ecf978fa3bb3bd
JoabOwala <owalajoab@gmail.com>
2023-10-04 16:48:24 +0300

    Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Nav from "./components/Nav";
+import "./style.css"
 
 function App() {
   return (
     <div>
-      <h1>AirBnB clone</h1>
+      <Nav />
     </div>
-  );
+    )
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../images/airbnb-logo.png"
+
+function Nav() {
+    return (
+
+    <nav>
+      <img src={logo} alt="logo" className="nav--logo"/>
+    </nav>
+
+)
+}
+
+export default Nav;
